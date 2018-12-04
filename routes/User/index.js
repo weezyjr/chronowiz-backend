dbd702b7cb0307882b62f6e81e5ffe477a975ed6
heiba-at-110136271225 <heiba@chronowiz.com>
2018-12-05 01:19:37 +0400

    Re-arranged routes

--- routes/User/index.js
+++ routes/User/index.js
@@ -3,8 +3,7 @@ let router = require('express').Router();
 router.use('/brands', require('./brands'));
 router.use('/collections', require('./collections'));
 router.use('/watches', require('./watches'));
-router.use('/search', require('./search'));
 
-router.use('/account', require('./account'));
+router.use('/search', require('./search'));
 
 module.exports = router;
