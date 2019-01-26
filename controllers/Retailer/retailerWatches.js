f42f5d28b8f33b06db08bfab924449feebda9afb
heiba-at-110136271225 <heiba@chronowiz.com>
2019-01-26 12:08:13 +0400

    Implemented conditions and checks for retailer to update the discount for any watch in his stock

--- controllers/Retailer/retailerWatches.js
+++ controllers/Retailer/retailerWatches.js
@@ -15,7 +15,7 @@ module.exports.AddToStockById = async function(req, res, next)
 
         let retailer = await Retailer.findById(req.user._id).populate('watchObjects.watchObject');
         if(!retailer)
-            return done({en: 'This Retailer is not registered', errorType: ErrorType.UNAUTHORIZED});
+            return res.json(Response.error({en: 'This Retailer is not registered', errorType: ErrorType.UNAUTHORIZED}));
 
         let watch = await Watch.findById(req.params._id);
         if(!watch)
@@ -45,7 +45,7 @@ module.exports.RemoveFromStockById = async function(req, res, next)
 
         let retailer = await Retailer.findById(req.user._id).populate('watchObjects.watchObject');
         if(!retailer)
-            return done({en: 'This Retailer is not registered', errorType: ErrorType.UNAUTHORIZED});
+            return res.json(Response.error({en: 'This Retailer is not registered', errorType: ErrorType.UNAUTHORIZED}));
 
         let watch = await Watch.findById(req.params._id);
         if(!watch)
@@ -55,7 +55,7 @@ module.exports.RemoveFromStockById = async function(req, res, next)
         if(!existingWatch)
             return res.json(Response.error({en: 'Watch is not present in retailer\'s stock watches.'}));
 
-        retailer.watchObjects.pull({"_id" : existingWatch._id});
+        retailer.watchObjects.pull({"_id": existingWatch._id});
 
         await retailer.save();
 
@@ -73,28 +73,82 @@ module.exports.UpdateRetailerWatchDiscount = async function(req, res, next)
     {
         Request.validateReq(req, {enforceParamsId: true, enforcePayload: true});
 
-        let retailerWatchDiscount =  Request.validatePercentage(req.body.payload.retailerWatchDiscount, 'retailerWatchDiscount', {optional: false});
+        let retailerWatchDiscount = Request.validatePercentage(req.body.payload.retailerWatchDiscount, 'retailerWatchDiscount', {optional: false});
 
-        let retailer = await Retailer.findById(req.user._id).populate('watchObjects.watchObject');
+        let retailer = await Retailer.findById(req.user._id)
+            .populate('watchObjects.watchObject')
+            .populate('maximumBrandDiscounts.brandObject')
+            .populate('maximumCollectionDiscounts.collectionObject')
+            .populate('maximumWatchDiscounts.watchObject');
+
         if(!retailer)
-            return done({en: 'This Retailer is not registered', errorType: ErrorType.UNAUTHORIZED});
+            return res.json(Response.error({en: 'This Retailer is not registered', errorType: ErrorType.UNAUTHORIZED}));
 
-        let watch = await Watch.findById(req.params._id);
+        let watch = await Watch.findById(req.params._id).populate('brandObject').populate('collectionObject');
         if(!watch)
             return res.json(Response.error({en: 'No watch is available with this Id.'}));
 
-        let existingWatch = retailer.watchObjects.find(retailerWatch => (retailerWatch.watchObject &&  retailerWatch.watchObject.referenceNumber === watch.referenceNumber));
+        let existingWatch = retailer.watchObjects.find(retailerWatch => (retailerWatch.watchObject && (retailerWatch.watchObject.referenceNumber === watch.referenceNumber)));
         if(!existingWatch)
-            return res.json(Response.error({en: 'Watch is not present in retailer\'s stock watches.'}));
+            return res.json(Response.error({en: 'Watch is not present in the retailer\'s stock watches.'}));
 
-        //TODO check against brand/collection/watch maximum discount
-        //TODO check against retailer discount
+        const setRetailerWatchDiscount = async function()
+        {
+            existingWatch.retailerWatchDiscount = retailerWatchDiscount;
+            await retailer.save();
+            return res.json(Response.payload({payload: retailer}));
+        };
 
-        existingWatch.retailerWatchDiscount = retailerWatchDiscount;
+        let retailerMaximumWatchDiscount = retailer.maximumWatchDiscounts.find(retailerMaximumWatchDiscount => (retailerMaximumWatchDiscount.watchObject && retailerMaximumWatchDiscount.watchObject._id.equals(watch._id)));
+        if(retailerMaximumWatchDiscount)
+        {
+            if(retailerMaximumWatchDiscount.maximumWatchDiscount >= retailerWatchDiscount)
+                return await setRetailerWatchDiscount();
+            else
+                return res.json(Response.error({en: 'The Maximum discount that can be applied on this watch for your retailer account is ' + retailerMaximumWatchDiscount.maximumWatchDiscount + '%'}));
+        }
 
-        await retailer.save();
+        let retailerMaximumCollectionDiscount = retailer.maximumCollectionDiscounts.find(retailerMaximumCollectionDiscount => (retailerMaximumCollectionDiscount.collectionObject && watch.collectionObject && retailerMaximumCollectionDiscount.collectionObject._id.equals(watch.collectionObject._id)));
+        if(retailerMaximumCollectionDiscount)
+        {
+            if(retailerMaximumCollectionDiscount.maximumCollectionDiscount >= retailerWatchDiscount)
+                return await setRetailerWatchDiscount();
+            else
+                return res.json(Response.error({en: 'The Maximum discount that can be applied on this collection for your retailer account is ' + retailerMaximumCollectionDiscount.maximumCollectionDiscount + '%'}));
+        }
 
-        return res.json(Response.payload({payload: retailer}));
+        let retailerMaximumBrandDiscount = retailer.maximumBrandDiscounts.find(retailerMaximumBrandDiscount => (retailerMaximumBrandDiscount.brandObject && watch.brandObject && retailerMaximumBrandDiscount.brandObject._id.equals(watch.brandObject._id)));
+        if(retailerMaximumBrandDiscount)
+        {
+            if(retailerMaximumBrandDiscount.maximumBrandDiscount >= retailerWatchDiscount)
+                return await setRetailerWatchDiscount();
+            else
+                return res.json(Response.error({en: 'The Maximum discount that can be applied on this brand for your retailer account is ' + retailerMaximumBrandDiscount.maximumBrandDiscount + '%'}));
+        }
+
+        if(watch.maximumDiscount)
+        {
+            if(watch.maximumDiscount >= retailerWatchDiscount)
+                return await setRetailerWatchDiscount();
+            else
+                return res.json(Response.error({en: 'The Maximum discount that can be applied on this watch is ' + watch.maximumDiscount + '%'}));
+        }
+
+        if(watch.collectionObject.maximumDiscount)
+        {
+            if(watch.collectionObject.maximumDiscount >= retailerWatchDiscount)
+                return await setRetailerWatchDiscount();
+            else
+                return res.json(Response.error({en: 'The Maximum discount that can be applied on this collection is ' + watch.collectionObject.maximumDiscount + '%'}));
+        }
+
+        if(watch.brandObject.maximumDiscount)
+        {
+            if(watch.brandObject.maximumDiscount >= retailerWatchDiscount)
+                return await setRetailerWatchDiscount();
+            else
+                return res.json(Response.error({en: 'The Maximum discount that can be applied on this brand is ' + watch.brandObject.maximumDiscount + '%'}));
+        }
     }
     catch(error)
     {
